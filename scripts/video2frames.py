02378c283a576c37c81666ff4863c7c6ae8c3afb
Andreas G. Petersen <s173901@student.dtu.dk>
2021-11-08 14:12:07 +0100

    splitted vidoes into frames

---
+++ scripts/video2frames.py
@@ -0,0 +1,46 @@
+# Program To Read video and Extract Frames from https://www.geeksforgeeks.org/python-program-extract-frames-using-opencv/
+
+import cv2
+import os
+
+# image dimensions
+width = 640
+height = 480
+dim = (width, height)
+  
+# Function to extract frames
+def saveFrame(paths):
+    # Path to video file
+    for i in range(len(paths)):
+        path = paths[i]
+        vidObj = cv2.VideoCapture(path)
+
+        print("Saving video",i+1)
+        success, image = vidObj.read()
+        count = 0
+
+        while success:
+
+            image = cv2.resize(image, dim)
+            filename = root + "vid"+str(i+1)+"f"+str(count)+".jpg"
+            
+            cv2.imwrite(filename, image)
+            
+            success, image = vidObj.read()
+            count += 1
+  
+# Driver Code
+if __name__ == '__main__':
+  
+    # Calling the function
+    root = "data/"
+    vid1_path = os.path.join(root, "video1.avi")
+    vid2_path = os.path.join(root, "video2.avi")
+    paths = [vid1_path, vid2_path]
+
+    print("Resizing and saving following files:")
+    print(vid1_path)
+    print(vid2_path)
+    print("Starting...")
+    saveFrame(paths)
+    print("Success")
