0bf2e12f7fe38dd929b0fa68b47bbe161ad6083c
Jonas <joni_brondum@protonmail.com>
2021-11-15 14:19:47 +0100

    Changed data-tree structure for test/train

--- scripts/split_data.py
+++ scripts/split_data.py
@@ -2,10 +2,12 @@
 import os
 import shutil 
 
-#setwd()
+#Setting local work directory as root of project
+os.chdir("C:/Sync/Dokumenter/Universitet/Master/7_semester/02456_Deep_learning/02456-project/")
 cwd = os.getcwd()
 data_path = cwd
-data = os.listdir(data_path)
+bb_path = "/data/video2/boundingboxes"
+data = os.listdir(data_path + bb_path)
 
 
 # Split files into train and test
@@ -14,22 +16,39 @@ from sklearn.model_selection import train_test_split
 train, test = train_test_split(data, test_size=0.2, random_state=1)
 
 # Create test and train directory
-train_path = cwd + "/data/video1/train"
-test_path = cwd + "/data/video1/test"
+train_path = cwd + "/data/video2/train"
+test_path = cwd + "/data/video2/test/"
+img_path = cwd + "/data/video2/frames/"
+
+
+train_box = train_path + "/boundingboxes"
+train_frames = train_path + "/frames"
+test_box = test_path + "/boundingboxes"
+test_frames = test_path + "/frames"
 
 try:
    os.mkdir(train_path)
    os.mkdir(test_path)
+   os.mkdir(train_box)
+   os.mkdir(train_frames)
+   os.mkdir(test_box)
+   os.mkdir(test_frames)
 except FileExistsError:
    pass
 
+
 # Move train files to train folder
 for file_name in train:
-   shutil.copy(os.path.join(data_path, file_name), train_path)
+   #Boundingboxes folder:
+   shutil.copy(os.path.join(data_path + bb_path, file_name), train_box)
+   #Images folder:
+   shutil.copy(os.path.join(img_path, file_name[:-4] + '.jpg'), train_frames)
+
 
 # Move test files to test folder
 for file_name in test:
-   shutil.copy(os.path.join(data_path, file_name), test_path)
+   shutil.copy(os.path.join(data_path + bb_path, file_name), test_box)
+   shutil.copy(os.path.join(img_path, file_name[:-4] + '.jpg'), test_frames)
 
 print("Size train data: ", len(train), "\n","Size test data: ", len(test))
 print(len(data))
