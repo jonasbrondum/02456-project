6a59c33de852102815cc6ec1c36f1b8bdfcb17c7
Jonas <joni_brondum@protonmail.com>
2021-11-15 14:40:59 +0100

    Generalized to let any PC use this script

--- scripts/split_data.py
+++ scripts/split_data.py
@@ -2,10 +2,13 @@
 import os
 import shutil 
 
-#Setting local work directory as root of project
-os.chdir("C:/Sync/Dokumenter/Universitet/Master/7_semester/02456_Deep_learning/02456-project/")
+#Use only this method if running directly from scripts folder otherwise comment out the 
+# following change.
+os.chdir('..')
+
 cwd = os.getcwd()
 data_path = cwd
+# Choose boundingbox path appropriately (video1 or video2)
 bb_path = "/data/video2/boundingboxes"
 data = os.listdir(data_path + bb_path)
 
