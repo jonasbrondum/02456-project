aaf931d665bb967ac2ce52e822071be32aecec8e
Jonas <joni_brondum@protonmail.com>
2021-11-15 15:59:43 +0100

    Added coke to data

--- scripts/split_data.py
+++ scripts/split_data.py
@@ -4,12 +4,13 @@ import shutil
 
 #Use only this method if running directly from scripts folder otherwise comment out the 
 # following change.
-os.chdir('..')
+# os.chdir('..')
 
 cwd = os.getcwd()
 data_path = cwd
 # Choose boundingbox path appropriately (video1 or video2)
-bb_path = "/data/video2/boundingboxes"
+video = 'video1'
+bb_path = "/data/" + video + "/boundingboxes/"
 data = os.listdir(data_path + bb_path)
 
 
@@ -19,9 +20,9 @@ from sklearn.model_selection import train_test_split
 train, test = train_test_split(data, test_size=0.2, random_state=1)
 
 # Create test and train directory
-train_path = cwd + "/data/video2/train"
-test_path = cwd + "/data/video2/test/"
-img_path = cwd + "/data/video2/frames/"
+train_path = cwd + "/data/" + video + "/train/"
+test_path = cwd + "/data/" + video + "/test/"
+img_path = cwd + "/data/" + video + "/frames/"
 
 
 train_box = train_path + "/boundingboxes"
