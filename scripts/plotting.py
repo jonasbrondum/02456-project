0a5706e50be884ed430279888129417d99c8a366
jonasbrondum <joni_brondum@protonmail.com>
2021-12-07 17:58:00 +0100

    Final custom plot script

--- scripts/plotting.py
+++ scripts/plotting.py
@@ -5,19 +5,22 @@ import matplotlib.pyplot as plt
 # plt.style.use('_mpl-gallery')
 
 
-annotations=["MobileNetv3","MobileNetv3_320","ResNet50","YOLOv5"]
+model=["MobileNetv3","MobileNetv3_320","ResNet50","YOLOv5"]
 # Creating a numpy array
-inferencetime = np.array([0.052, 0.05, 0.3238, 0.03])
-accuracy = np.array([65.3, 61.2, 86.1, 85.1])
+inferencetime = np.array([0.0874, 0.0495, 0.3238, 0.72])
+accuracy = np.array([87.4, 27.3, 86.1, 85.1])
 # Plotting point using sactter method
 plt.figure(figsize=(8,6))
-plt.scatter(inferencetime,accuracy,s=50,color="maroon")
+plt.scatter(inferencetime,accuracy,s=100,color="maroon")
 plt.grid()
-plt.xlim([0, 0.4])
-plt.xlabel("Inference time [s]")
-plt.ylabel("mean Averaged Precision (mAP) [%]")
+plt.xlim([0, 0.8])
+plt.ylim([25, 95])
+plt.xlabel("Inference time [s]", fontsize = 14)
+plt.ylabel("mean Averaged Precision (mAP) [%]", fontsize = 14)
 plt.title("Accuracy vs inference time",fontsize=15)
-for i, label in enumerate(annotations):
-    plt.annotate(label, (inferencetime[i], accuracy[i]))
-plt.savefig('plots/accvsinf.eps', pad_inches = 0.1)
+plt.annotate(model[0], (inferencetime[0] - 0.01, accuracy[0] + 1.5), fontsize = 14)
+plt.annotate(model[1], (inferencetime[1] + 0.01, accuracy[1] - 0.01), fontsize = 14)
+plt.annotate(model[2], (inferencetime[2] - 0.05, accuracy[2] - 3.5), fontsize = 14)
+plt.annotate(model[3], (inferencetime[3] - 0.1, accuracy[3] - 3.5), fontsize = 14)
+plt.savefig('plots/accvsinf.png', pad_inches = 0.1)
 plt.show()
