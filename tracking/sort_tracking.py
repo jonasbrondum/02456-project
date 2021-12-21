be9b079f6923b0c18b59a7dfd3805de0cf06c323
Andreas G. Petersen <s173901@student.dtu.dk>
2021-12-21 19:03:32 +0100

    SORT tracker added

--- tracking/sort_tracking.py
+++ tracking/sort_tracking.py
@@ -1,4 +1,4 @@
-from pyimagesearch.centroidtracker import CentroidTracker
+
 from imutils.video import VideoStream
 from imutils.video import FPS
 import numpy as np
@@ -7,19 +7,22 @@ import imutils
 import torch
 import time
 import cv2
+from sort import *
 
+#create instance of SORT
+mot_tracker = Sort() 
+track_bbs_ids = []
 frames = -1
-ct = CentroidTracker()
 
-CONFIDENCE = 0.8
+CONFIDENCE = 0.95
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 CLASSES = ['background','beer','cola']
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 # PARSE YOUR MODEL HERE:
 MODEL_PATH = "/home/andreasgp/MEGAsync/DTU/9. Semester/Deep Learning/object-tracking-project/02456-project/models/mobilenetv3_large_320_15epochs_entire_dataset.pth"
-# PARSE YOUR VIDEO HERE:"
-#VID_SOURCE = "C:/Users/Philip/02456-project/data/2021_10_28_12_49_00.avi" # 0 is webcam
-VID_SOURCE = "/home/andreasgp/Documents/02456-project-21-12-21/02456-project/data/2021_10_28_12_49_00.avi" # 0 is webcam
+
+# PARSE YOUR VIDEO HERE:
+VID_SOURCE = "/home/andreasgp/Documents/02456-project-21-12-21/02456-project/data/2021_10_28_12_49_00.avi" #livevideo1.MOV"#2021_10_28_12_49_00.avi"
 #model = torch.hub.load('ultralytics/yolov5', 'custom', path=MODEL_PATH)
 model = torch.load(MODEL_PATH,map_location=torch.device('cpu'))
 model = model.to(DEVICE)
@@ -64,6 +67,7 @@ while True:
     with torch.no_grad():
         detections = model(frame)[0]
     rects = []
+    valid_box = []
 
     # loop over the detections
     for i in range(0, len(detections["boxes"])):
@@ -87,17 +91,39 @@ while True:
             y = startY - 15 if startY - 15 > 15 else startY + 15
             cv2.putText(orig, label, (startX, y),
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+            valid_box.append(box)
 
 	# update our centroid tracker using the computed set of bounding
 	# box rectangles
-    objects = ct.update(rects)
+    #print(detections)
+    
+    #print(valid_box)
+    dets = []
+    for box in valid_box:
+        det = [box[0],box[1],box[2],box[3]]
+        dets.append(det)
+   # print(dets)
+    #print(np.array(dets)[0,:])
+    if len(dets)>0:
+        track_bbs_ids = mot_tracker.update(np.array(dets))
+#    print(track_bbs_ids)
+
 
 	# loop over the tracked objects
-    for (objectID, centroid) in objects.items():
-            text = "ID {}".format(objectID)
-            cv2.putText(orig, text, (centroid[0] - 10, centroid[1] - 10),
-			    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            cv2.circle(orig, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+    for i in range(len(track_bbs_ids)):
+        #print(track_bbs_ids[i])
+        centroid = track_bbs_ids[i,:4]
+        #print(centroid)
+        id = str(int(track_bbs_ids[i,-1]))
+        #print(id)
+        text = "ID {}".format(id)
+        #print(text)
+        #print(centroid[0] - 10, centroid[1] - 10)
+        #cv2.putText(orig, text, (100+i*40,100),
+        #    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.putText(orig, text, (int(abs(centroid[0] - 10)), int(abs(centroid[1] - 10))),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.circle(orig, (int(centroid[0]), int(centroid[1])), 4, (0, 255, 0), -1)
 
 
 
@@ -110,7 +136,7 @@ while True:
         fpsMin = compute_time
     #cv2.putText(orig, "FPS : " + str(float(compute_time)), (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,255), 2);
     # show the output frame
-    print("Frame:",frames)
+    print("Frame:", frames)
     cv2.imshow("Frame", orig)
     key = cv2.waitKey(0) & 0xFF
     # if the 'q' key was pressed, break from the loop
@@ -126,3 +152,5 @@ print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 print("[INFO] interval FPS: [{:.2f} - {:.2f}]".format(fpsMin, fpsMax))
 # do a bit of cleanup
 cv2.destroyAllWindows()
+
+
